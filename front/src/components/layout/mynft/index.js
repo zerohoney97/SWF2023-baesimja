45b18b16a67b74b1db55d9ca18fee76794524ba1
zerohoney <zerohoney97@gmail.com>
2023-08-02 05:47:58 +0900

    final

--- front/src/components/layout/mynft/index.js
+++ front/src/components/layout/mynft/index.js
@@ -1,47 +1,108 @@
-import React from 'react'
-import { Top, MyNftBox, Collection } from './Mynft.styled'
+import React, { useEffect, useState } from "react";
 
-import nft1 from '../../img/nft1.png'
-import nft2 from '../../img/nft2.png'
-import nft3 from '../../img/nft3.png'
-import right from '../../img/right.png'
+import { Top, MyNftBox, Collection } from "./Mynft.styled";
+import { mintJusticeTokenContract } from "../../layout/search/abiConfig";
+
+import nft1 from "../../img/nft1.png";
+import nft2 from "../../img/nft2.png";
+import nft3 from "../../img/nft3.png";
+import right from "../../img/right.png";
 
 const MyNftTop = () => {
-    let user_id = 'blah'
-    let nft_num = 10;
+  let user_id = "blah";
+  let nft_num = 10;
 
-    return (
-        <>
-        <Top>{user_id}'s Collections ({nft_num})</Top>
-        </>
-    )
-}
+  return (
+    <>
+      <Top>
+        {user_id}'s Collections ({nft_num})
+      </Top>
+    </>
+  );
+};
 
-const MyCollection = () => {
-    return (
-        <>
-        <Collection>
-            <img src={nft1}></img>
-            <p className='nft-info'>nft이름 어쩌구asfsadasfasdfdsfasdfafsd</p>
-            <p className='case-info'>판례 정보 어쩌구</p>
-            <img className='goto' src={right}></img>
-        </Collection>
-        </>
-    )
-}
+const MyCollection = ({ value, index }) => {
+  return (
+    <>
+      <Collection>
+        <img src={nft1} alt={value.img}></img>
+        <p className="nft-info">{value.caseName}</p>
+        <p className="case-info">{value.caseNum}</p>
+        <p>{value.date}</p>
+        <p>{value.sentence}</p>
+      </Collection>
+    </>
+  );
+};
 
 const MyNftMid = () => {
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // 월렛 계정 가져오기
+  const [account, setAccount] = useState("");
+  const [nftInfo, setNftInfo] = useState("");
+  const [nftArr, setNftArr] = useState([]);
+  const rawNftToObject = () => {
+    const tempArr = [...nftArr];
+    nftInfo["0"].forEach((el, index) => {
+      tempArr.push({
+        img: nftInfo["0"][index],
+        caseNum: nftInfo["1"][index],
+        caseName: nftInfo["2"][index],
+        date: nftInfo["3"][index],
+        sentence: nftInfo["4"][index],
+      });
+    });
+    setNftArr(tempArr);
+  };
+  useEffect(() => {
+    if (nftInfo !== "") {
+      rawNftToObject();
+    }
+  }, [nftInfo]);
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) {
+        const [accounts] = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(accounts);
+      } else {
+        console.log(window.ethereum);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getAccount();
+  });
+  useEffect(() => {
+    console.log(account);
+  }, [account]);
+  // 민팅 정보 가져오기
+  const getNftInfo = async () => {
+    const nftInfo = await mintJusticeTokenContract.methods
+      .getAllUserNft(account)
+      .call();
+    console.log(nftInfo);
+    console.log(await mintJusticeTokenContract.methods.getMsgSender().call());
+    setNftInfo(nftInfo);
+  };
+  useEffect(() => {
+    if (account !== "") {
+      getNftInfo();
+    }
+  }, [account]);
+  return (
+    <>
+      <MyNftBox>
+        {nftArr.length !== 0 &&
+          nftArr.map((value, index) => {
+            return <MyCollection value={value} index={index} />;
+          })}
+      </MyNftBox>
+    </>
+  );
+};
 
-    return (
-        <>
-        <MyNftBox>
-            {arr.map((value, index) => {
-                return <MyCollection key={index} />
-            })}
-        </MyNftBox>
-        </>
-    )
-}
-
-export {MyNftTop, MyNftMid}
+export { MyNftTop, MyNftMid };
